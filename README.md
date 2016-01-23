62a58c9b5690be2e7cb60268af67451a426c1e18
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2016-01-23 18:21:43 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#Squiz Matrix EES Image Preview Plugin - v1.0
+
+#Dependencies
+
+1. Squiz Matrix
+2. Squiz Matrix Easy Edit Suite
+
+##Setup
+
+1. include eeplus-image-preview.js at the bottom of your EES head code
+2. then call setThumb('http://www.yoursite.com');
---
+++ eeplus-image-preview.js
@@ -0,0 +1,28 @@
+EasyEdit.plugins.imagePreview = {
+    init: function() {
+        var self = this;
+        EasyEditEventManager.bind('AssetFinderAfterInit', function() {
+            $(document).on("click", "#ees_assetFinderBody a",
+                function(e) {
+                    setThumb();
+                });
+        });
+    }
+};
+
+function setThumb(siteURL) {
+    if ($('#ees_assetFinderBody .selected .assetIcon').hasClass("image")) {
+        $('#preview-image').remove();
+        var assetidsel = $('.currentSelectionId').text();
+        var assetidrem = $.trim(assetidsel);
+        var assetid = assetidrem.replace('#', '');
+        var asseturl = siteURL + '?a=' + assetid;
+        var assethtml =
+            '<div style="border:1px solid #000; width:200px;" id="preview-image"><img src="' +
+            asseturl + '" width="200"/></div>';
+        $('#ees_assetFinderFooter').append(assethtml);
+    }
+    if (!$('#ees_assetFinderBody .selected .assetIcon').hasClass("image")) {
+        $('#preview-image').remove();
+    }
+}
